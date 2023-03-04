16c002d6764bb75b80df10f0bd409cd834a9ae9a
dhnguy <NGUYENHDAVID@HOTMAIL.COM>
2023-03-04 03:35:42 +0000

    impement cloudwatch logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -27,9 +27,23 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProces
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+#cloudwatch
+import watchtower
+import logging
+from time import strftime
+
+# Configuring Logger to Use CloudWatch
+#LOGGER = logging.getLogger(__name__)
+#LOGGER.setLevel(logging.DEBUG)
+#console_handler = logging.StreamHandler()
+#cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+#LOGGER.addHandler(console_handler)
+#LOGGER.addHandler(cw_handler)
+#LOGGER.info("test log")
+
 #xray
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+#xray_url = os.getenv("AWS_XRAY_URL")
+#xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -47,7 +61,7 @@ tracer = trace.get_tracer(__name__)
 app = Flask(__name__)
 
 #xray
-XRayMiddleware(app, xray_recorder)
+#XRayMiddleware(app, xray_recorder)
 
 # HoneyComb initialize automatic instrumentation with flask
 FlaskInstrumentor().instrument_app(app)
@@ -63,6 +77,12 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+#@app.after_request
+#def after_request(response):
+   # timestamp = strftime('[%Y-%b-%d %H:%M]')
+   # LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+   # return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
@@ -101,7 +121,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  data = HomeActivities.run(logger= LOGGER)
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
